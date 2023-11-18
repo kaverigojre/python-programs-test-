ef8e67e24564e837fe0c30f122e6ea1e327f47ff
Kaveri Gojare <kaverigojre@gmail.com>
2023-11-18 15:45:44 +0530

    Added square root values in main method

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -12,3 +12,13 @@ class Calculator:
     def square_root(self, x):
         return math.sqrt(x)
 
+if __name__ == "__main__":
+    calculator = Calculator()
+    num1 = 16
+    num2 = 4
+    num3= 25
+    print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
+    print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
+    print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
+    print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+    print(f"The square root of {num3} = {calculator.square_root(num3)}")
